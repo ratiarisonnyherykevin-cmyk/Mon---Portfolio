8dfff77ae3d374dcf8cfe45dbb27d34c2afcfbbe
ratiarisonnyherykevin-cmyk <ratiarisonnyherykevin@gmail.com>
2026-03-02 09:03:39 +0300

    add

---
+++ java.js
@@ -0,0 +1,12 @@
+﻿const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const triggerPoint = window.innerHeight - 100;
+
+        if (sectionTop < triggerPoint) {
+            section.classList.add("show");
+        }
+    });
+});
